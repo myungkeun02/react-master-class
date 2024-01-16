54a4655006586e7c3dac246fe1a8dde913e1e798
myungkeun <audrms3568@gmail.com>
2024-01-17 00:08:20 +0900

    #2.3 'As' and Attrs

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import styled from "styled-components";
 
 const Father = styled.div`
   display: flex;
-`;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+  flex-direction: column;
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Btn = styled.button`
+  margin: 10px;
+  color: white;
+  background-color: tomato;
+  border: 0;
+  width: 50px;
+  height: 20px;
+  border-radius: 15px;
+  text-align: center;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  margin: 10px;
+  background-color: tomato;
+  width: 100px;
+  height: 20px;
+  border: 0px;
+  border-radius: 5px;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="red" />
-      <Circle bgColor="blue" />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Btn>Log in</Btn>
+      <Btn as="a" href="https://www.youtube.com">
+        go
+      </Btn>
     </Father>
   );
 }
