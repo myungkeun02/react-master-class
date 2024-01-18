bc5338fd61302a9f9924cfc98dfe881fb2c8992b
myungkeun <audrms3568@gmail.com>
2024-01-18 21:33:52 +0900

    #3.5 Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+    console.log(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value );
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow"></Circle>
-      <Circle bgColor="tomato"></Circle>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        ></input>
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
