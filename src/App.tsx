95750ca8ad43e7657aeceabd801471faf59ce2db
myungkeun <audrms3568@gmail.com>
2024-01-22 23:20:10 +0900

    %4.1 Browser Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
-import { useState } from "react";
-import styled, { ThemeProvider, keyframes } from "styled-components";
-import { darkTheme } from "./theme";
-import { lightTheme } from "./theme";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin: 0px;
-`;
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-  font-size: 100px;
-`;
-const Btn = styled.button`
-  background-color: ${(props) => props.theme.btnColor};
-  color: ${(props) => props.theme.textColor};
-  width: 200px;
-  height: 100px;
-  font-size: 40px;
-`;
+import React from "react";
+import { Route } from "react-router";
 
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <Container>
-        <Title>Hello!</Title>
-        <Btn>dark theme</Btn>
-      </Container>
-    </ThemeProvider>
+    <div>
+      <Route />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { lightTheme } from "./theme";
-import { darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    
-      <App />
-    
+    <App />
   </React.StrictMode>
 );
