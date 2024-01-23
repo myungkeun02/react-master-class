9948d0705e8c98d43fb20c8d59c9b288c7c58e8d
myungkeun <audrms3568@gmail.com>
2024-01-24 00:18:41 +0900

    #5.0 Setup

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,39 +1,20 @@
-import { createBrowserRouter } from "react-router-dom";
-import Root from "./Root";
-import About from "./screens/About";
-import Home from "./screens/Home";
-import NotFound from "./screens/NotFound";
-import ErrorComponent from "./components/ErrorComponent";
-import User from "./screens/users/User";
-import Followers from "./screens/users/Followers";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Coins from "./routes/Coins";
+import Coin from "./routes/Coin";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-        errorElement: <ErrorComponent />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "users/:userId",
-        element: <User />,
-        children: [
-          {
-            path: "followers",
-            element: <Followers />,
-          },
-        ],
-      },
-    ],
-    errorElement: <NotFound />,
-  },
-]);
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/:coinId">
+          <Coin />
+        </Route>
+        <Route path="/">
+          <Coins />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
-export default router;
+export default Router;
