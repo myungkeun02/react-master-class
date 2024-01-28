1b8f3e8424b228b958840b02f3a7ff591420f47e
myungkeun <audrms3568@gmail.com>
2024-01-28 15:39:05 +0900

    #5.5 coin data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
@@ -33,13 +33,30 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<Params>();
   const { state } = useLocation<RouteState>();
-
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const response = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json;
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading === true ? <Loader>Loading...</Loader> : null}
+      {loading === true ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <span>{}</span>
+      )}
     </Container>
   );
 }
---
+++ src/screens/Home.tsx
@@ -0,0 +1,5 @@
+function Home() {
+  const users: any = [];
+  return <h1>{users[0].name}</h1>;
+}
+export default Home;
