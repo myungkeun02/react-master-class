977bcebffbb6fcc30411a04f375fd4b667e78ae3
myungkeun <audrms3568@gmail.com>
2024-02-18 21:14:26 +0900

    #8.2 Basic Animations

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -10,7 +10,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
@@ -21,9 +21,11 @@ const Box = styled.div`
 function App() {
   return (
     <Wrapper>
-      <Box />
-      <div></div>
-      <motion.div></motion.div>
+      <Box
+        transition={{ type: "spring", damping: 5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
     </Wrapper>
   );
 }
