85d8c816db08094e73482e95c0afbd222d4fe4de
myungkeun <audrms3568@gmail.com>
2024-02-18 23:15:26 +0900

    #8.3 Variants part One

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -11,6 +11,7 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
+  margin: 20px;
   width: 200px;
   height: 200px;
   background-color: white;
@@ -18,14 +19,15 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", damping: 0.5 } },
+};
+
 function App() {
   return (
     <Wrapper>
-      <Box
-        transition={{ type: "spring", damping: 5 }}
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
-      />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
